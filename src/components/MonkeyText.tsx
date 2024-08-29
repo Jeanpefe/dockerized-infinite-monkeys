83c80d8917881a99a9099d8ba05fa6d2107ee3b7
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-08-29 23:37:24 +0200

    refactor: monkeytextprops to separated interface

--- src/components/MonkeyText.tsx
+++ src/components/MonkeyText.tsx
@@ -1,6 +1,13 @@
 import { useGenerateText } from "../hooks/useGenerateText"
 
-export default function MonkeyText({ inputText, generate, charactersPerSecond }: { inputText: string, generate: boolean, charactersPerSecond: number }) {
+interface MonkeyTextProps {
+	inputText: string;
+	generate: boolean;
+	charactersPerSecond: number;
+	onTextFound: () => void;
+  }
+
+export default function MonkeyText({ inputText, generate, charactersPerSecond, onTextFound }: MonkeyTextProps) {
 	const { displayText } = useGenerateText(generate, charactersPerSecond)
 	return (
 		<section style={{ "background": "#404040", "maxWidth": "90rem", "borderRadius": "0.5rem", "overflowX": "auto", "padding": "0.5rem 1rem" }} >
