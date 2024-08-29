688d4d9e598fcbda2dea231101ddba5411d450db
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-08-29 23:55:06 +0200

    chore: generate text inside of setDisplayText to handle inputText inclusion avoiding asyncronous issues

--- src/hooks/useGenerateText.tsx
+++ src/hooks/useGenerateText.tsx
@@ -3,18 +3,20 @@ import { CHARACTERS } from "../consts";
 
 export const useGenerateText = (generate: boolean, charactersPersSecond: number, inputText:string, onTextFound: () => void) => {
 	const [displayText, setDisplayText] = useState('')
+	const characters = CHARACTERS;
+	const charactersLength = characters.length;
 
 	useEffect(() => {
 		if (generate) {
 			setDisplayText('')
 			const interval = setInterval(() => {
-				const characters = CHARACTERS;
-				const charactersLength = characters.length;
-				const newText = displayText + characters.charAt(Math.floor(Math.random() * charactersLength))
-				if (newText.includes(inputText)) {
-					onTextFound();
-				  }
-				setDisplayText(prevDisplayText => prevDisplayText + characters.charAt(Math.floor(Math.random() * charactersLength)))
+				setDisplayText(prev => {
+					const newText = prev + characters.charAt(Math.floor(Math.random() * charactersLength))
+					if (newText.includes(inputText)) {
+						onTextFound();
+					  }
+					return newText
+				})
 			}, 1000 / charactersPersSecond);
 			// Limpiar el intervalo cuando el componente se desmonte o 'generate' cambie
 			return () => clearInterval(interval);
