2b958bee39b32cc9f17479a7317aa1fa054f82c3
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-09-03 22:06:28 +0200

    feat: display "random" texts variations in the footer as if it has been typed by monkeys

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
+
 export default function Footer() {
+    const [currentText, setCurrentText] = useState('Made by Jeanpefe');
+
+    useEffect(() => {
+
+        const texts = [
+            'Mdae by Ajenpfe',
+            'Made by Jeanpefe',
+            'Made bJeapnfe',
+            'Mad by Je1npefe',
+            'Made by Jeapfene',
+            'Mdae by Jeanpef',
+            'Mdae b Jeanp3f',
+            'Mdae by Jenaepefe',
+            'M4ae by Jenaepefe'
+        ];
+
+        const intervalId = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * texts.length);
+            setCurrentText(texts[randomIndex]);
+        }, 1500);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <footer>
-            <p>Made by Jeanpefe</p>
+            <p>{currentText}</p>
         </footer>
-    )
+    );
 }
