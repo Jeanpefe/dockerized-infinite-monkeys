e14c51a0807774f1f13875dd76e277e694afc5d2
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-09-20 23:56:32 +0200

    feat: math explanation with katex dependency

--- src/components/MathExplanation.tsx
+++ src/components/MathExplanation.tsx
@@ -1,13 +1,16 @@
+import { BlockMath, InlineMath } from 'react-katex'
+import 'katex/dist/katex.min.css';
+
 export default function MathExplanation() {
 	return (
 	  <section>
 		<h2>Mathematical Explanation</h2>
 		<p>
-		  Let's say that our keyboard has 26 different keys. The probability of a monkey typing a word of length <em>L</em> is (1/26)^L.<br/>
-		  Hence, the probability of not typing the word of length <em>L</em> in a 26 character attempt is $1-(\frac{1}{26})^L$.<br />
-		  Since each block of length <em>L</em> is independently typed, we can conclude that the probability of not typing a word of length <em>L</em> after the <em>n</em>-th block of length <em>L</em> pressed is<br />
-		  P_n = \left(1-\left(\frac{1}{26}\right)^L\right)^n$ which tends to 0 as <em>n</em> goes to infinity.
-		</p>
+      Let's say that our keyboard has 26 different keys. The probability of a monkey typing a word of length <em>L</em> is <InlineMath math="(1/26)^L" />.<br />
+      Hence, the probability of not typing the word of length <em>L</em> in a 26 character attempt is <InlineMath math="1 - \left(\frac{1}{26}\right)^L" />.<br />
+      Since each block of length <em>L</em> is independently typed, we can conclude that the probability of not typing a word of length <em>L</em> after the <em>n</em>-th block of length <em>L</em> pressed is<br />
+      <InlineMath math="P_n = \left(1-\left(\frac{1}{26}\right)^L\right)^n" /> which tends to 0 as <em>n</em> goes to infinity.
+    </p>
 	  </section>
 	);
   }
