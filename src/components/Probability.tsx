a5d530243268250ebe07aa47540b206111b525a7
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-09-21 20:05:23 +0200

    feat: display probability in kpis with less text

--- src/components/Probability.tsx
+++ src/components/Probability.tsx
@@ -8,7 +8,36 @@ interface ProbabilityUnderConditionsProps {
 	numberOfDecimals: number;
 }
 export function ProbabilityUnderConditions({ inputNumberOfMonkeys, numberOfCharacters, displayTextNumberOfCharacters, inputText, numberOfDecimals }: ProbabilityUnderConditionsProps) {
-	return (
-		<p>Probability of typing the word in less than {displayTextNumberOfCharacters} characters with {inputNumberOfMonkeys} {inputNumberOfMonkeys > 1 ? 'monkeys' : 'monkey'}: {getProbabilityOfTypingAWordWithMultipleMonkeys({ inputNumberOfMonkeys, numberOfCharacters, inputText, numberOfDecimals })}</p>
-	)
+	const probability = getProbabilityOfTypingAWordWithMultipleMonkeys({
+		inputNumberOfMonkeys,
+		numberOfCharacters,
+		inputText,
+		numberOfDecimals
+	  });
+	
+	  return (
+		<div style={{
+		  display: 'flex',
+		  flexDirection: 'column',
+		  alignItems: 'center',
+		  justifyContent: 'center',
+		  backgroundColor: '#ffe0b2',
+		  padding: '1.5rem',
+		  borderRadius: '12px',
+		  textAlign: 'center'
+		}}>
+		  <h3 style={{
+			fontSize: '1.2rem',
+			color: '#5d4037',
+		  }}>
+			{probability}
+		  </h3>
+		  <p style={{
+			fontSize: '1.2rem',
+			color: '#6d4c41'
+		  }}>
+			Probability in {displayTextNumberOfCharacters} characters
+		  </p>
+		</div>
+	  )
 }
